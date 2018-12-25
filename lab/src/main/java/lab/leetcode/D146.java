9a56b0d2674c97cf673f9fe293e8d5c0bdbade00
songjisong <songjisong@foxmail.com>
2018-12-25 13:37:41 +0800

    新增 leetcode 题目

---
+++ lab/src/main/java/lab/leetcode/D146.java
@@ -0,0 +1,47 @@
+package lab.leetcode;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author jasonsong
+ * 2018/12/24
+ */
+
+
+public class D146 {
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        lruCache.put(1, 1);
+        lruCache.put(3, 3);
+        lruCache.put(4, 4);
+        System.out.println(lruCache.get(1));
+        System.out.println(lruCache.get(2));
+        System.out.println(lruCache.get(3));
+        System.out.println(lruCache.get(4));
+    }
+
+    static class LRUCache {
+        public int capacity;
+        public LinkedHashMap<Integer, Integer> map;
+
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            map = new LinkedHashMap<>(capacity, 0.75F, true);
+        }
+
+        public int get(int key) {
+            return map.getOrDefault(key, -1);
+        }
+
+        public void put(int key, int value) {
+            map.remove(key);
+            if(map.size() >= capacity){
+                map.remove(map.keySet().iterator().next());
+            }
+            map.put(key, value);
+        }
+    }
+}
