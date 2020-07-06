fb914ed30a83add46adda36c685baeb84fac43d3
jason <songjisong@souche.com>
2020-07-06 21:13:33 +0800

    add some answers

---
+++ lab/src/main/java/lab/leetcode/dailytask/M63.java
@@ -0,0 +1,78 @@
+package lab.leetcode.dailytask;
+
+/**
+ * 63. 不同路径 II
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * <p>
+ * <p>
+ * <p>
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 说明：m 和 n 的值均不超过 100。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ * [0,0,0],
+ * [0,1,0],
+ * [0,0,0]
+ * ]
+ * 输出: 2
+ * 解释:
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ *
+ * @author jasonsong
+ * 2020/7/6
+ */
+
+
+public class M63 {
+    public static void main(String[] args) {
+        int[][] obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        int[][] obstacleGrid1 = {
+                {0, 0, 0, 0}
+                , {0, 0, 1, 0}
+                , {0, 1, 0, 0}
+                , {0, 0, 0, 0}};
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+        System.out.println(uniquePathsWithObstacles(obstacleGrid1));
+    }
+
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = obstacleGrid[0].length;
+        if (n == 0) {
+            return 0;
+        }
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i + j == 2) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if (obstacleGrid[i - 1][j - 1] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+}
