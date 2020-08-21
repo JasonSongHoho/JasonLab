fe1a2d00673425c0ef223b964fc162cf7a69731d
jason <songjisong@souche.com>
2020-08-21 22:43:35 +0800

    add some labs

---
+++ lab/src/main/java/lab/test/spi/DubboSpiTest.java
@@ -0,0 +1,24 @@
+package lab.test.spi;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+
+/**
+ * @author jasonsong
+ * 2020/8/21
+ */
+
+
+public class DubboSpiTest {
+    public static void main(String[] args) throws Exception {
+        sayHello();
+    }
+
+    public static void sayHello() throws Exception {
+        ExtensionLoader<Robot> extensionLoader =
+                ExtensionLoader.getExtensionLoader(Robot.class);
+        Robot optimusPrime = extensionLoader.getExtension("optimusPrime");
+        optimusPrime.sayHello();
+        Robot bumblebee = extensionLoader.getExtension("bumblebee");
+        bumblebee.sayHello();
+    }
+}
--- lab/src/main/java/lab/test/spi/Robot.java
+++ lab/src/main/java/lab/test/spi/Robot.java
@@ -1,10 +1,13 @@
 package lab.test.spi;
 
+import com.alibaba.dubbo.common.extension.SPI;
+
 /**
  * @author jasonsong
  * 2020/8/21
  */
 
+@SPI
 public interface Robot {
     void sayHello();
 }
