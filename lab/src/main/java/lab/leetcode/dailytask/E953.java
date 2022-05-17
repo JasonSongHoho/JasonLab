e416d60582c45e15719416637284ca0799f04f1e
jason <xiaojie@xiaohongshu.com>
2022-05-17 23:20:20 +0800

    leetCode

---
+++ lab/src/main/java/lab/leetcode/dailytask/E953.java
@@ -0,0 +1,68 @@
+package lab.leetcode.dailytask;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 953. 验证外星语词典
+ * 某种外星语也使用英文小写字母，但可能顺序 order 不同。字母表的顺序（order）是一些小写字母的排列。
+ *
+ * 给定一组用外星语书写的单词 words，以及其字母表的顺序 order，只有当给定的单词在这种外星语中按字典序排列时，返回 true；否则，返回 false。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+ * 输出：true
+ * 解释：在该语言的字母表中，'h' 位于 'l' 之前，所以单词序列是按字典序排列的。
+ * 示例 2：
+ *
+ * 输入：words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+ * 输出：false
+ * 解释：在该语言的字母表中，'d' 位于 'l' 之后，那么 words[0] > words[1]，因此单词序列不是按字典序排列的。
+ * 示例 3：
+ *
+ * 输入：words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+ * 输出：false
+ * 解释：当前三个字符 "app" 匹配时，第二个字符串相对短一些，然后根据词典编纂规则 "apple" > "app"，因为 'l' > '∅'，其中 '∅' 是空白字符，定义为比任何其他字符都小（更多信息）。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 20
+ * order.length == 26
+ * 在 words[i] 和 order 中的所有字符都是英文小写字母。
+ *
+ * @author Jason/XiaoJie
+ * @date 2022/5/17
+ */
+public class E953 {
+    public boolean isAlienSorted(String[] words, String order) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < order.length(); i++) {
+            map.put(order.charAt(i), i);
+        }
+        for (int i = 0; i < words.length - 1; i++) {
+            String wordLeft = words[i];
+            String wordRight = words[i + 1];
+            boolean finishLoop = true;
+            for (int j = 0; j < wordLeft.length() && j < wordRight.length(); j++) {
+                char cL = wordLeft.charAt(j);
+                char cR = wordRight.charAt(j);
+                int diff = map.get(cL) - map.get(cR);
+                if (diff < 0) {
+                    finishLoop = false;
+                    break;
+                } else if (diff > 0) {
+                    return false;
+                }
+            }
+            if (finishLoop && wordLeft.length() > wordRight.length()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
