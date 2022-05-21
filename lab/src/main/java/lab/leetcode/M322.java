f04bb5501eacabc418953aa5796a90d8bc098a4a
jason <xiaojie@xiaohongshu.com>
2022-05-22 01:37:44 +0800

    add leetCodes

---
+++ lab/src/main/java/lab/leetcode/M322.java
@@ -0,0 +1,56 @@
+package lab.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 322. 零钱兑换
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * <p>
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * <p>
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
+ * <p>
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 231 - 1
+ * 0 <= amount <= 104
+ *
+ * @author Jason/XiaoJie
+ * @date 2022/5/22
+ */
+public class M322 {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
