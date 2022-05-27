6a8f4d4ee2ce44a3ec82b341717a446c62fb7315
jason <xiaojie@xiaohongshu.com>
2022-05-27 10:35:34 +0800

    add leetCodes

--- lab/src/main/java/lab/leetcode/dailytask/MSJD_M_17_11.java
+++ lab/src/main/java/lab/leetcode/dailytask/MSJD_M_17_11.java
@@ -10,16 +10,11 @@ import java.util.List;
  */
 public class MSJD_M_17_11 {
     public static void main(String[] args) {
-//        List<Integer> positionList1 =  new ArrayList<>(Arrays.asList(1,3,5));
-//
-//        int i1 = Collections.binarySearch(positionList1, 2);
-//        System.out.println(i1);
-//        System.out.println(Collections.binarySearch(positionList1, 4));
 
         System.out.println("" + findClosest(new String[]{"I", "am", "a", "student", "from", "a", "university", "in", "a", "city"}, "a", "from"));
     }
 
-    public static int findClosest(String[] words, String word1, String word2) {
+    public static int findClosest1(String[] words, String word1, String word2) {
         List<Integer> positionList1 = new ArrayList<>();
         List<Integer> positionList2 = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
@@ -48,4 +43,34 @@ public class MSJD_M_17_11 {
         }
         return distance;
     }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode.cn/problems/find-closest-lcci/solution/dan-ci-ju-chi-by-leetcode-solution-u96o/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param words
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public static int findClosest(String[] words, String word1, String word2) {
+        int length = words.length;
+        int ans = length;
+        int index1 = -1, index2 = -1;
+        for (int i = 0; i < length; i++) {
+            String word = words[i];
+            if (word.equals(word1)) {
+                index1 = i;
+            } else if (word.equals(word2)) {
+                index2 = i;
+            }
+            if (index1 >= 0 && index2 >= 0) {
+                ans = Math.min(ans, Math.abs(index1 - index2));
+            }
+        }
+        return ans;
+    }
+
 }
