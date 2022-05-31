fcc3e48ac5cccfb5ab7bb06455c2d934ab2a1770
jason <xiaojie@xiaohongshu.com>
2022-05-31 14:28:44 +0800

    add leetCodes

---
+++ lab/src/main/java/lab/leetcode/common/Trie.java
@@ -0,0 +1,100 @@
+package lab.leetcode.common;
+
+/**
+ * E208
+ * 208. 实现 Trie (前缀树)
+ * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+ * <p>
+ * 请你实现 Trie 类：
+ * <p>
+ * Trie() 初始化前缀树对象。
+ * void insert(String word) 向前缀树中插入字符串 word 。
+ * boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+ * boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * 输出
+ * [null, null, true, false, true, null, true]
+ * <p>
+ * 解释
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 True
+ * trie.search("app");     // 返回 False
+ * trie.startsWith("app"); // 返回 True
+ * trie.insert("app");
+ * trie.search("app");     // 返回 True
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= word.length, prefix.length <= 2000
+ * word 和 prefix 仅由小写英文字母组成
+ * insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
+ *
+ * @author Jason/XiaoJie
+ * @date 2022/5/31
+ */
+public class Trie {
+    Trie[] children;
+    boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("test");
+        System.out.println(trie.search("tes"));
+        System.out.println(trie.search("test"));
+    }
+
+    public void insert(String word) {
+        int index;
+        Trie cur = this;
+        for (int i = 0; i < word.length(); i++) {
+            index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                cur.children[index] = new Trie();
+            }
+            cur = cur.children[index];
+        }
+        cur.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return startsWithPre(word, true);
+    }
+
+    public boolean startsWith(String prefix) {
+        return startsWithPre(prefix, false);
+    }
+
+    public boolean startsWithPre(String prefix, boolean checkIsEnd) {
+        int index;
+        Trie cur = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            index = prefix.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                return false;
+            }
+            cur = cur.children[index];
+        }
+        return !checkIsEnd || cur.isEnd;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
--- lab/src/main/java/lab/leetcode/dailytask/M46.java
+++ lab/src/main/java/lab/leetcode/dailytask/M46.java
@@ -1,98 +1,40 @@
 package lab.leetcode.dailytask;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
- * <p>
- * abcdefghijk
- * 012345678910
- * <p>
- * l  m  n  o  p  q  r  s  t  u
- * 11 12 13 14 15 16 17 18 19 20
- * <p>
- * v  w  x  y  z
- * 21 22 23 24 25
- * <p>
- * 示例 1:
- * <p>
- * 输入: 12258
- * 输出: 5
- * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
- *  
- * <p>
- * 提示：
- * <p>
- * 0 <= num < 231
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- * <p>
- * 122123343123121321417
- *
- * @author jasonsong
- * 2020/6/9
+ * @author Jason/XiaoJie
+ * @date 2022/5/31
  */
-
-
-@Slf4j
 public class M46 {
-    static int sum = 0;
+    static List<List<Integer>> ans;
 
     public static void main(String[] args) {
-//        sum = 0;
-//        System.out.println(translateNum2(12258));
-//        System.out.println(sum);
-//        System.out.println();
-//        sum = 0;
-//        System.out.println(translateNum2(1225));
-//        System.out.println(sum);
-//        System.out.println();
-
-
-        System.out.println(translateNum2(12));
-        System.out.println(translateNum2(1222));
-        System.out.println(translateNum2(1225));
-
+        System.out.println(permute(new int[]{1, 2, 3}));
     }
 
-    public static int translateNum2(int num) {
-        if (num < 10) {
-            return 1;
-        }
-        if (num % 100 < 10 || num % 100 > 25) {
-            return translateNum2(num / 10);
-        } else {
-            return translateNum2(num / 10) + translateNum2(num / 100);
-        }
+    public static List<List<Integer>> permute(int[] nums) {
+        ans = new ArrayList<>();
+        List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        dfs(new ArrayList<>(), numsList);
+        return ans;
     }
 
-    public static int translateNum(int num) {
-        String numStr = "" + num;
-        int numLength = numStr.length();
-        int[] dp = new int[numLength + 1];
-        dp[0] = 1;
-
-        for (int i = 0; i < numLength; i++) {
-            dp[i + 1] += dp[i];
-            if (i > 0) {
-                if (numStr.charAt(i - 1) != '0' && numStr.substring(i - 1, i + 1).compareTo("25") <= 0) {
-                    dp[i + 1] += dp[i - 1];
-                }
-            }
+    private static void dfs(List<Integer> list, List<Integer> leftNums) {
+        int n = leftNums.size();
+        if (n == 0) {
+            ans.add(list);
         }
-        return dp[numLength];
-    }
-
-    public int translateNum1(int num) {
-        // 递归，两位两位地看
-        if (num < 10) return 1;
-        // 如果末两位形如01，或者33，这种只能读一位
-        if (num % 100 < 10 || num % 100 > 25)
-            return translateNum(num / 10);
-        // 如果末两位形如 12， 可以有两种读法，读2， 或者读12
-        return translateNum(num / 10) + translateNum(num / 100);
+        for (int i = 0; i < n; i++) {
+            List<Integer> subList = new ArrayList<>(list);
+            List<Integer> subLeftList = new ArrayList<>(leftNums);
+            subList.add(leftNums.get(i));
+            subLeftList.remove(i);
+            dfs(subList, subLeftList);
+        }
     }
 
 }
