51fae18b146f0d341e9514d650d94ce03f785b39
jason <xiaojie@xiaohongshu.com>
2022-06-07 00:26:27 +0800

    add leetCodes

---
+++ lab/src/main/java/lab/leetcode/dailytask/M875.java
@@ -0,0 +1,72 @@
+package lab.leetcode.dailytask;
+
+/**
+ * 875. 爱吃香蕉的珂珂
+ * 珂珂喜欢吃香蕉。这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 h 小时后回来。
+ *
+ * 珂珂可以决定她吃香蕉的速度 k （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 k 根。如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+ *
+ * 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+ *
+ * 返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：piles = [3,6,7,11], h = 8
+ * 输出：4
+ * 示例 2：
+ *
+ * 输入：piles = [30,11,23,4,20], h = 5
+ * 输出：30
+ * 示例 3：
+ *
+ * 输入：piles = [30,11,23,4,20], h = 6
+ * 输出：23
+ *
+ *
+ * 提示：
+ *
+ * 1 <= piles.length <= 104
+ * piles.length <= h <= 109
+ * 1 <= piles[i] <= 109
+ *
+ * @author Jason/XiaoJie
+ * @date 2022/6/7
+ */
+public class M875 {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = piles[0];
+        int len = piles.length;
+        for (int i = 1; i < len; i++) {
+            int pile = piles[i];
+            max = Math.max(pile, max);
+        }
+        int l = 1, r = max, m = max;
+        while (l <= r) {
+            m = l + ((r - l) >> 1);
+            long time = getTime(piles, m);
+            if (time > h) {
+                l = m + 1;
+            } else {
+                if (m == 1 || getTime(piles, m - 1) > h) {
+                    return m;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return m;
+    }
+
+
+    public long getTime(int[] piles, int speed) {
+        long time = 0;
+        for (int pile : piles) {
+            int curTime = (pile + speed - 1) / speed;
+            time += curTime;
+        }
+        return time;
+    }
+}
